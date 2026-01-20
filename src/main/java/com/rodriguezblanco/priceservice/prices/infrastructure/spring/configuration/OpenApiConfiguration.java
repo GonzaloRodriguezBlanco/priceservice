a57aa5b5028718796445a290c3f4b71dd647f6e8
Gonzalo Rodriguez <gonzalo@rodriguez-blanco.com>
2026-01-20 15:42:09 +0100

    refactor: Delete code to serve static OpenApi documentation

--- src/main/java/com/rodriguezblanco/priceservice/prices/infrastructure/spring/configuration/OpenApiConfiguration.java
+++ src/main/java/com/rodriguezblanco/priceservice/prices/infrastructure/spring/configuration/OpenApiConfiguration.java
@@ -5,30 +5,11 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.configuration.SpringDocConfiguration;
-import org.springdoc.core.properties.SpringDocConfigProperties;
-import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class OpenApiConfiguration {
-    /*
-    // https://springdoc.org/#what-is-a-proper-way-to-set-up-swagger-ui-to-use-provided-spec-yml
-    @Bean
-    SpringDocConfiguration springDocConfiguration(){
-        return new SpringDocConfiguration();
-    }
-
-    @Bean
-    SpringDocConfigProperties springDocConfigProperties() {
-        return new SpringDocConfigProperties();
-    }
-
-    @Bean
-    ObjectMapperProvider objectMapperProvider(SpringDocConfigProperties springDocConfigProperties){
-        return new ObjectMapperProvider(springDocConfigProperties);
-    }*/
 
     @Bean
 
