e89f67885e1bf678359be8836b48d83056ab73c8
Gonzalo Rodriguez <gonzalo@rodriguez-blanco.com>
2026-01-22 14:27:50 +0100

    fix: Improve validation messages for bad responses

--- src/main/java/com/rodriguezblanco/priceservice/prices/user/rest/GetProductPriceInDateController.java
+++ src/main/java/com/rodriguezblanco/priceservice/prices/user/rest/GetProductPriceInDateController.java
@@ -9,14 +9,20 @@ import com.rodriguezblanco.priceservice.support.application.userports.QueryBus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.Explode;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,16 +55,15 @@ public class GetProductPriceInDateController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ProductPriceResponse get(
-            @Valid
             @Parameter(description = "The unique identifier for a product",
                     schema = @Schema(implementation = ProductKey.class), explode = Explode.TRUE)
             @PathVariable
             ProductKey productKey,
-            @Valid
             @Parameter(description = "The date on which the price is checked",
                     schema = @Schema(type = "string", format = "date-time", example = "2020-06-14T10:00:00Z"))
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
             @PathVariable
+            @PastOrPresent
             LocalDateTime date
     ) {
         return RestMapper.toResponse(
